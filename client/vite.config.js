07927a0ba862915a04d2df6b1b755a9c84c6cc11
Jason-W-K <jaybashee139@gmail.com>
2025-11-06 21:21:52 +0300

    Add Vite frontend with GitHub Pages config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mern-stack-integration-Jason-W-K/', // 👈 my repo name
+  base: '/deployment-and-devops-essentials-Jason-W-K/'
 });
